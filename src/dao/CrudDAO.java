4ddcd5609bd3238ac5964b03648530125465baed
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-05-31 13:03:45 +0530

    CrudDAO name change

---
+++ src/dao/CrudDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import entity.SuperEntity;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO<T> extends SuperEntity {
+    public ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+    public boolean add(T dto) throws SQLException, ClassNotFoundException;
+    public boolean update(T dto) throws SQLException, ClassNotFoundException;
+    public boolean exist(String id) throws SQLException, ClassNotFoundException;
+    public String generateNewID() throws SQLException, ClassNotFoundException;
+    public boolean delete(String id) throws SQLException, ClassNotFoundException;
+    public T search(String id) throws SQLException, ClassNotFoundException;
+
+}
